34c9081b53a0af1f9239d6bf50577bc7b60abf57
deyyybid <deybid.eyron21@gmail.com>
2025-07-01 14:13:42 +0800

    Add Day One File for Python Lesson

---
+++ Day_One.py
@@ -0,0 +1,25 @@
+# Printing Statement
+print("Hello, World!")
+print("My name is David Aaron Del Rosario.")
+
+# String Manipulation
+print("Hello, World!\nHello, World!\nHello, World!")
+
+# Input Function
+print(f"Hello {input("What is your name? ")}!")
+
+# Variables ft. Length
+username = input("Name: ")
+length = len(username)
+
+print(f"Length of username: {length}")
+
+
+
+# Final Task
+print("Welcome to the Band Name Generator")
+
+cityName = input("What's the name of the city you grew up in?\n")
+
+petName = input("What's your pet's name?\n")
+print(f"Your band name could be {cityName} {petName}")
